f04c1af575cd83e4236e68de791613e1841663f4
MatheusCFBT <matheusterzini@gmail.com>
2026-03-29 12:43:18 -0300

    Refactor equation solver feature: remove old files and add new structure with routes, controllers, and UI components

---
+++ lib/features/equation_solver/presentation/camera/equation_solver_camera_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class EquationSolverCameraPage extends StatefulWidget {
+  const EquationSolverCameraPage({super.key})
+
+  @override
+  State<EquationSolverCameraPage> createState() => _EquationSolverCameraPageState();
+} 
+
+class _EquationSolverCameraPageState extends State<EquationSolverCameraPage> {
+  late final controller = widget.controller;
+  
+  @override
+  Widget build(BuildContext) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ lib/features/equation_solver/routes.dart
@@ -0,0 +1,12 @@
+import 'package:go_router/go_router.dart';
+
+abstract final class EquationSolverRoutes {
+  static const _base = 'home/equation_solver';
+  static const calculator = '$_base/calculator';   
+  static const cameraCalculator = '$_base/camera/calculator';   
+}
+
+// final EquationSolverRoutes = [
+//   GoRoute(
+//     path: path)
+// ]
